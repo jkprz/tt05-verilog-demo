0577b45ed111b29c21a891e64b50439c96a30e6e
J <jprzepioski@hotmail.com>
2023-11-04 01:11:47 -0700

    Revert

--- src/tt_um_jkprz.v
+++ src/tt_um_jkprz.v
@@ -16,17 +16,22 @@ module tt_um_jkprz (
 
         // use bidirectionals as outputs
         assign uio_oe = 8'b11111111;
-        assign uio_out[5:0] = 6'd0;
+        //assign uio_out[5:0] = 6'd0;
+        assign uio_out[6:0] = 7'd0;
 
+        /*
         if(!ena) // error unhappy with not using ena or uio_in
         begin
             junk <= uio_in;
         end
+        */
 
         // instantiate lif neuron
         //lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
         lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .beta(8'b00000011), .spike(uio_out[7]), .state(uo_out));
-        lif lif2(.current(ui_in), .clk(clk), .rst_n(rst_n), .beta(8'b00000011), .spike(uio_out[6]), .state(uo_out));
+
+        // Trying to add more, but it's not happy with this.
+        //lif lif2(.current(ui_in), .clk(clk), .rst_n(rst_n), .beta(8'b00000011), .spike(uio_out[6]), .state(uo_out));
         // lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
 
 endmodule
