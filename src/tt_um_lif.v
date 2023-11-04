61cbf2cbc4e08178e944b16549ec7173b9e86a47
J <jprzepioski@hotmail.com>
2023-11-03 22:40:17 -0700

    Add files via upload

--- src/tt_um_lif.v
+++ src/tt_um_lif.v
@@ -21,9 +21,7 @@ module tt_um_lif (
     //lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
 
     // known good below switching to lif_network next
-    //lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .beta(8'b11111111), .spike(uio_out[7]), .state(uo_out));
+    lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .beta(8'b11111111), .spike(uio_out[7]), .state(uo_out));
     // lif lif2(.current({uio_out[7], 7'b0000000}), .clk(clk), .rst_n(rst_n), .spike(uio_out[6]), .state(uo_out));
-
-    lif_network lif_network1(.current(ui_in), .spike_in(uio_out[7]), .spike_out(uo_out), .clk(clk), .rst_n(rst_n));
 
 endmodule
