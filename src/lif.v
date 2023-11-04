44f57791904ceab45bceb350024b76d6d8278bc1
J <jprzepioski@hotmail.com>
2023-11-03 22:17:26 -0700

    Try to shift by 2 bit beta (can add up to 3)

--- src/lif.v
+++ src/lif.v
@@ -4,7 +4,7 @@ module lif (
     input wire [7:0] current,
     input wire       clk,
     input wire       rst_n,
-    input reg beta, // weighting not in original code
+    input reg [1:0] beta, // weighting not in original code
     output wire      spike,
     output reg [7:0] state
 );
@@ -38,7 +38,7 @@ module lif (
     // next state = Ut+1, and state = Ut.  This equation is Ut+1 = I + Ut * beta where beta is the weighting
     // Example code he shift uses (state >> 1)+(state >> 2)+(state >> 3) which gives 0.5+0.25+0.125=0.875 but I will change the weights for STDP
     //[OLD] assign next_state = (spike ? 0 : current) + (spike ? 0 : (state >> 1)+(state >> 2)+(state >> 3));
-    assign next_state = (spike ? 0 : current) + (spike ? 0 : beta*(state >> 1)); // big weight
+    assign next_state = (spike ? 0 : current) + (spike ? 0 : (state >> beta)); // big weight
     //assign next_state = (spike ? 0 : current) + (spike ? 0 : (state >> 1)+(state >> 2)+(state >> 3));
 
 endmodule
